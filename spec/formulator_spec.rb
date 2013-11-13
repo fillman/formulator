823c3e9f220fad8f09580bf9bc5f948dacb02871
fillman <fila.luka@gmail.com>
2013-11-13 18:43:55 +0200

    more examples

--- spec/formulator_spec.rb
+++ spec/formulator_spec.rb
@@ -3,5 +3,8 @@ require 'spec_helper'
 describe Formulator do
   it 'should evaluate mathematical expression' do
     Formulator('5+3').should eql(8)
+    Formulator('(5+3)*2').should eql(16)
+    Formulator('(5+3)*2 + 1').should eql(17)
+    Formulator('5 + 6 / 2').should eql(8.0)
   end
 end
